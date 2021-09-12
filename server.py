44874710f8cd03fbce6ebfc24e2f02c3b08aa99e
Stepan Skriabin <stepan.skrjabin@gmail.com>
2021-09-12 12:06:53 +0300

    fix(server): minor fixed

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import configparser
 
 from flask import Flask
-from flask import render_template
+from flask import render_template, url_for
 import sqlobject as orm
 
 # ************** Logging beginning *******************
@@ -15,9 +15,10 @@ logging.disable()
 config = configparser.ConfigParser()
 config.read("config.ini")
 database = config['DATABASE']
+LOG = config['LOGGING']
 
 # loguru logger on
-add_logging(logging.getint("level"))
+add_logging(LOG.getint("level"))
 
 
 # Подключаемся к БД
@@ -32,6 +33,23 @@ finally:
 app = Flask(__name__)
 
 
-@app.route('/')
-def start_page():
-    return render_template("index.html")
+@app.route("/")
+@app.route('/home')
+def home():
+    classes = ("1А", "1Б", "1В",
+               "2А", "2Б", "2В", "2Г", "2Д",
+               "3А", "3Б", "3В", "3Г",
+               "4А", "4Б", "4В",
+               "5А", "5Б", "5В",
+               "6А", "6Б", "6В",
+               "7А", "7Б", "7В",
+               "8А", "8Б", "8В",
+               "9А", "9Б", "9В",
+               "10",
+               "11")
+    return render_template("index.html", classes=classes)
+
+
+@app.route('/schedules/<name>')
+def schedules(name):
+    return render_template("schedules.html", classname=name)
