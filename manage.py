a7cb81bef2db2774a85da0b5964c19501d5efcda
Stepan Skriabin <stepan.skrjabin@gmail.com>
2021-11-04 23:15:54 +0300

    feat(manage): added key to control schedules in crontab

--- manage.py
+++ manage.py
@@ -4,15 +4,18 @@ import configparser
 import click
 
 from models import dbhandler
+from controller import schedules
 
 # ************** Read "config.ini" ********************
 config = configparser.ConfigParser()
 config.read('config.ini')
 logging = config['LOGGING']
 DB = config["DATABASE"]
+SCHEDULES = config["SCHEDULES"]
 # ************** END **********************************
 
 db = dbhandler.DBHandler(DB.get('uri'))
+sc = schedules.Schedule(SCHEDULES.get('hour'))
 
 @click.command()
 @click.option("--database",
@@ -20,7 +23,12 @@ db = dbhandler.DBHandler(DB.get('uri'))
               help='Create or delete all table '
               'with all data. Оperation, '
               'by default, creates all tables')
-def main(database):
+@click.option("--schedules",
+              type=click.Choice(["run", "delete"]),
+              help='Run or delete cron job '
+              'where started parser.py with period'
+              'Perion settings in config.ini')
+def main(database, schedules):
     if database == "create":
         start_time = process_time()
         db.create()
@@ -31,6 +39,10 @@ def main(database):
         db.delete()
         click.echo(f'Table is deleted at: '
                    f'{process_time() - start_time} sec.')
+    elif schedules == "run":
+        click.echo(f"{sc.create()} and {sc.run()}")
+    elif schedules == "delete":
+        click.echo(f"{sc.delete()}")
 
 
 if __name__ == "__main__":
