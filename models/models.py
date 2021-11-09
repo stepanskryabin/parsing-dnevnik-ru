80f5ae4ef6c9007225a811399a7cfc2f73c807fc
Stepan Skriabin <stepan.skrjabin@gmail.com>
2021-11-09 21:21:21 +0300

    fix(models): fixed DB models default table config

--- models/models.py
+++ models/models.py
@@ -11,8 +11,8 @@ class Classes(orm.SQLObject):
         в системе dnevnik.ru
         timetable ([MultipleJoin]): привязка к записи содержащей расписание
     """
-    name = orm.StringCol(notNone=True)
-    dnevnik_id = orm.IntCol()
+    name = orm.StringCol(notNone=True, unique=True)
+    dnevnik_id = orm.IntCol(notNone=True, unique=True)
     # Связь с другими таблицами
     timetable = orm.MultipleJoin('Timetable')
 
@@ -30,11 +30,11 @@ class Timetable(orm.SQLObject):
         lesson_teacher ([str]): ФИО учителя, который проводит урок
         classes ([ForeignKey]): привязка к названию учебного класса
     """
-    date = orm.StringCol()
-    lesson_number = orm.IntCol()
-    lesson_name = orm.StringCol()
-    lesson_room = orm.StringCol()
-    lesson_teacher = orm.StringCol()
-    lesson_time = orm.StringCol()
+    date = orm.StringCol(notNone=True)
+    lesson_number = orm.IntCol(notNone=True)
+    lesson_name = orm.StringCol(default=None)
+    lesson_room = orm.StringCol(default=None)
+    lesson_teacher = orm.StringCol(default=None)
+    lesson_time = orm.StringCol(default=None)
     # Связь с другими таблицами
     classes = orm.ForeignKey('Classes')
