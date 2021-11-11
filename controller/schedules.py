593e4c55ef48f270e4ec29ecb64db3a349b0adf2
Stepan Skriabin <stepan.skrjabin@gmail.com>
2021-11-11 21:52:04 +0300

    feat(schedules): added function to create win_cron.tab

--- controller/schedules.py
+++ controller/schedules.py
@@ -1,4 +1,5 @@
 from os import name as OS_NAME
+from os import path
 
 from crontab import CronTab
 
@@ -11,17 +12,31 @@ class Schedule:
         self.period = period
         self._comment = "update_db"
         self._path = "/code/parsing-dnevnik-ru/"
-        self._tabfile = tabfile
+        self.tabfile = tabfile
+        self._path = path.abspath("")
 
-        if OS_NAME == 'posix':
+        if OS_NAME == 'posix1':
             self._cron = CronTab(user=True)
             self._command = "".join((f"cd $HOME{self._path} && "
                                          "pipenv run python ",
                                          f"$HOME{self._path}parser.py"))
-        elif OS_NAME == 'nt':
-            if self._tabfile is None:
-                self._tabfile = 'win_cron.tab'
-            self._cron = CronTab(tabfile=self._tabfile)
+        elif OS_NAME == 'posix':
+            if self.tabfile is None:
+                self.tabfile = 'win_cron.tab'
+
+            file_exists = path.exists(self.tabfile)
+            if file_exists:
+                self.tabfile = path.join(self._path,
+                                          self.tabfile)
+            else:
+                filename = "".join((self._path,
+                                    "/",
+                                    self.tabfile))
+                with open(filename, "w") as f:
+                    f.write("")
+                    f.close()
+
+            self._cron = CronTab(tabfile=self.tabfile)
             self._command = "".join(("python ",
                                      "$PARSER/parser.py"))
 
@@ -36,7 +51,7 @@ class Schedule:
         if OS_NAME == "posix":
             self._cron.write_to_user(user=True)
         elif OS_NAME == "nt":
-            self._cron.write(self._tabfile)
+            self._cron.write(self.tabfile)
         return "Job created"
 
     def run(self) -> str:
