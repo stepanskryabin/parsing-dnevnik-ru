d6ea0b29976ee4e6d7ade31f5ee747c46afc7e29
Stepan Skriabin <stepan.skrjabin@gmail.com>
2022-05-21 11:30:12 +0300

    fixed(convtime): namedtuple convert to NamedTuple class, add docstring, add typing to function parameters and returned result, correct several errors.

--- controller/convtime.py
+++ controller/convtime.py
@@ -1,4 +1,4 @@
-from collections import namedtuple
+from typing import NamedTuple
 from datetime import timedelta
 from datetime import date
 
@@ -8,25 +8,37 @@ from controller.config import DNEVNIK_RU
 from controller.config import TRIMESTER
 
 
+class Date(NamedTuple):
+    name: str
+    date: date
+    str_date: str
+
+
 def filtred_by_week(year: int,
                     month: int,
                     day: int,
                     deep_day: int) -> set:
-    """Выводит список двух дат принадлежащих разным неделям
+    """
+    Выводит список двух дат принадлежащих разным неделям.
+
+    Даты могут быть любыми, главный критерий это обязательная принадлежность
+    к разным неделям.
 
     Args:
-        year (int): год начала
-        month (int): месяц начала
-        day (int): день начала
-        deep_day (int): глубина (дни) на которую загружается расписание
+        year: год начала
+        month: месяц начала
+        day: день начала
+        deep_day: глубина (дни) на которую загружается расписание
 
     Returns:
-        set: список дат, которые принадлежат только одной неделе.
+        список дат, которые принадлежат только одной неделе.
         На одну неделю - одна дата.
     """
+
     start_date = date(year, month, day)
     list_of_day = [timedelta(day) for day in range(deep_day)]
     list_of_date = [start_date + day for day in list_of_day]
+
     result = []
     new_week = list_of_date[0]
     result.append(new_week)
@@ -39,18 +51,20 @@ def filtred_by_week(year: int,
     return set(result)
 
 
-def date_on_week(today=None,
-                 week: int = None) -> tuple:
-    """Выводит список дат с понедельника по воскресенье
-    используя для этого только текущую дату
+def date_on_week(today: date = None,
+                 week: int = None) -> tuple[Date]:
+    """
+    Выводит список дат с понедельника по воскресенье
+    используя для этого только текущую дату.
 
     Args:
-        today (date): текущая дата
-        week (int): номер недели, где 1 - текущая, 2 следующая
+        today: текущая дата
+        week: номер недели, где 1 - текущая, 2 следующая
 
     Returns:
-        tuple: список дат с понедельника по воскресенье
+        список дат с понедельника по воскресенье
     """
+
     if today is None:
         TODAY = date.today()
     else:
@@ -60,9 +74,7 @@ def date_on_week(today=None,
         WEEK = 1
     else:
         WEEK = week
-    Date = namedtuple("Date", ["name",
-                               "date",
-                               "str_date"])
+
     WEEKDAY_NAME = ("ПН",
                     "ВТ",
                     "СР",
@@ -70,15 +82,17 @@ def date_on_week(today=None,
                     "ПТ",
                     "СБ",
                     "ВС")
+    NUMBER_LIST = [0,
+                   1,
+                   2,
+                   3,
+                   4,
+                   5,
+                   6]
+
     WEEKDAY = date.weekday(TODAY)
-    a = [0,
-         1,
-         2,
-         3,
-         4,
-         5,
-         6]
-    FIRST_DAY = TODAY - timedelta(days=a.index(WEEKDAY))
+    FIRST_DAY = TODAY - timedelta(days=NUMBER_LIST.index(WEEKDAY))
+
     result = []
     if WEEK == 1:
         for day in range(7):
@@ -97,15 +111,16 @@ def date_on_week(today=None,
 
 
 def convert_to_isodate(from_string: str) -> str:
-    """Функция конвертирует найденный ID в дату
-    в ISO-формате
+    """
+    Конвертирует найденный ID в дату в ISO-формате.
 
     Args:
-        from_string (str): строка содержащая дату, например 'd20201101'
+        from_string: строка содержащая дату, например 'd20201101'.
 
     Returns:
-        [str]: возвращает дату в формате ISO, например '2020-11-01'
+        Возвращает дату в формате ISO, например '2020-11-01'.
     """
+
     var = from_string.lstrip('d')
     return date(int(var[0:4]),
                 int(var[4:6]),
@@ -113,6 +128,17 @@ def convert_to_isodate(from_string: str) -> str:
 
 
 def get_trimester(request_date: date) -> int:
+    """
+    Определяет номер триместра.
+
+    Args:
+        request_date: дата
+
+    Returns:
+        Возвращает номер триместра в системе dnevnik.ru
+        Номера всех триместров задаются в config.ini
+    """
+
     number_of_trimester = (DNEVNIK_RU.getint('1trimester'),
                            DNEVNIK_RU.getint('2trimester'),
                            DNEVNIK_RU.getint('3trimester'))
@@ -126,6 +152,6 @@ def get_trimester(request_date: date) -> int:
     elif second_trimester < request_date <= third_trimester:
         return number_of_trimester[2]
     else:
-        logger.error(' '.join("Ошибка в определении триместра.",
-                              "Использован номер первого триместра"))
+        logger.error(''.join(("Ошибка в определении триместра. ",
+                              "Использован номер первого триместра")))
         return number_of_trimester[1]
